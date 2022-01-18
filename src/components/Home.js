7f53474b06b70d52da986113bbc05d5309950cea
Muhammad Ibn Saeed <mi.saeed@yahoo.com>
2022-01-18 15:29:35 +0600

    add

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <h1>Hello From the other side!! </h1>
+            <h1>Hello From the other side!!
+                and edited with VS code </h1>
         </div>
     )
 }
