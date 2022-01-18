be6c5b1e030abda143aae6b4ec26acd083d5d027
Muhammad Ibn Saeed <mi.saeed@yahoo.com>
2022-01-18 15:43:56 +0600

    adding login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import Home from './components/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function login() {
+    return (
+        <div>
+            <h1>This is login Form</h1>
+        </div>
+    )
+}
