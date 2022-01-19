8b96cae7c0aef6dd3a8c1c8cd4caaf2c2259bcc1
Muhammad Ibn Saeed <mi.saeed@yahoo.com>
2022-01-19 08:03:28 +0600

    adding left section

---
+++ src/components/MainBody/Feed.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LeftSection from "./LeftSection";
+
+function Feed() {
+  return (
+    <div className="main_container">
+      <div className="container">
+        <div className="body_container">
+          <LeftSection />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
--- src/components/MainBody/Home.js
+++ src/components/MainBody/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
-import NavBar from "./NavBar";
+import Feed from "./Feed";
+// import NavBar from "./NavBar";
 
 
 function Home() {
   return (
     <div>
       <h2>Hello Form the Home Page UPDATE!!</h2>
+      <Feed />
     </div>
   );
 }
