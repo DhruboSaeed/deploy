98987a04c495f62ac8486d61ce90a0bf34436fa5
Muhammad Ibn Saeed <mi.saeed@yahoo.com>
2022-01-19 11:19:47 +0600

    redu app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login/Login";
-import HomePage from "./components/MainBody/Home";
+import HomePage from './components/MainBody/Home'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route exact path="https://kind-bose-c112f6.netlify.app/" element={<Login />} />
-          <Route path="https://kind-bose-c112f6.netlify.app/home" element={<HomePage />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<HomePage/>} />
         </Routes>
       </div>
     </Router>
